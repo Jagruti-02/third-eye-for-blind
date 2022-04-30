cfd14a28baa930e96ab26325379aee2a53352a11
Jagruti <jagrutirkm@gmail.com>
2022-04-30 17:08:30 +0530

    ino update

--- Wifi_ESP32cam.ino
+++ Wifi_ESP32cam.ino
@@ -1,20 +1,27 @@
 #include <WebServer.h>
 #include <WiFi.h>
 #include <esp32cam.h>
+#define echoPin 7 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 6 //attach pin D3 Arduino to pin Trig of HC-SR04
 
-// ESTE PROGRAMA ENVIA IMAGEN SI SE COLOCA EN IP WEB, PERO SI SE COLOCA EN PYTHON ENVIA VIDEO POR LAS ITERACIONES. . . (SI FUNCIONA EN PYTHON)
-const char* WIFI_SSID = "RNRED";
-const char* WIFI_PASS = "A542256b7";
-
-WebServer server(80); //servidor en el puerto 80
-
-static auto loRes = esp32cam::Resolution::find(320, 240); //baja resolucion
-static auto hiRes = esp32cam::Resolution::find(800, 600); //alta resolucion 
-//static auto hiRes = esp32cam::Resolution::find(640, 480); //alta resolucion  (para tazas de fps) (IP CAM APP)
-
-
-void
-serveJpg() //captura imagen .jpg
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+const int red=11;
+const int blue=10;
+const int buzzer=5;
+int green=9;
+const int vibrationMotor=3;
+const char* WIFI_SSID = "Not available";
+const char* WIFI_PASS = "abc*1385";
+ 
+WebServer server(80);
+ 
+ 
+static auto loRes = esp32cam::Resolution::find(320, 240);
+static auto midRes = esp32cam::Resolution::find(350, 530);
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+void serveJpg()
 {
   auto frame = esp32cam::capture();
   if (frame == nullptr) {
@@ -24,39 +31,49 @@ serveJpg() //captura imagen .jpg
   }
   Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
                 static_cast<int>(frame->size()));
-
+ 
   server.setContentLength(frame->size());
   server.send(200, "image/jpeg");
   WiFiClient client = server.client();
-  frame->writeTo(client);  // y envia a un cliente (en este caso sera python)
+  frame->writeTo(client);
 }
-
-void
-handleJpgLo()  //permite enviar la resolucion de imagen baja
+ 
+void handleJpgLo()
 {
   if (!esp32cam::Camera.changeResolution(loRes)) {
     Serial.println("SET-LO-RES FAIL");
   }
   serveJpg();
 }
-
-void
-handleJpgHi() //permite enviar la resolucion de imagen alta
+ 
+void handleJpgHi()
 {
   if (!esp32cam::Camera.changeResolution(hiRes)) {
     Serial.println("SET-HI-RES FAIL");
   }
   serveJpg();
 }
-
-
-
-void
-setup()
+ 
+void handleJpgMid()
 {
+  if (!esp32cam::Camera.changeResolution(midRes)) {
+    Serial.println("SET-MID-RES FAIL");
+  }
+  serveJpg();
+}
+ 
+ 
+void  setup(){
+  
   Serial.begin(115200);
+  pinMode(red,OUTPUT);
+  pinMode(blue,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(buzzer, OUTPUT); // Sets Buzzer as the output
+  pinMode(vibrationMotor, OUTPUT); // Sets Buzzer as the output
   Serial.println();
-
   {
     using namespace esp32cam;
     Config cfg;
@@ -64,33 +81,71 @@ setup()
     cfg.setResolution(hiRes);
     cfg.setBufferCount(2);
     cfg.setJpeg(80);
-
+ 
     bool ok = Camera.begin(cfg);
-    Serial.println(ok ? "CAMARA OK" : "CAMARA FAIL");
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
   }
-
   WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS); //nos conectamos a la red wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-
   Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/cam-lo.jpg");//para conectarnos IP res baja
-
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/cam-hi.jpg");//para conectarnos IP res alta
-
-  server.on("/cam-lo.jpg",handleJpgLo);//enviamos al servidor
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cam-hi.jpg");
+  Serial.println("  /cam-mid.jpg");
+ 
+  server.on("/cam-lo.jpg", handleJpgLo);
   server.on("/cam-hi.jpg", handleJpgHi);
-
+  server.on("/cam-mid.jpg", handleJpgMid);
+ 
   server.begin();
 }
-
+ 
 void loop()
 {
-  server.handleClient();
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+
+  
+  Serial.print(distance);
+  Serial.print("Distance");
+  Serial.println();
+  
+  if(distance<15){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,LOW);
+    digitalWrite(buzzer,HIGH);
+    digitalWrite(vibrationMotor,HIGH);
+    server.handleClient();
+  }
+  else if (distance>15 && distance<30){
+    digitalWrite(red,LOW);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,HIGH); 
+    digitalWrite(buzzer,LOW);
+    digitalWrite(vibrationMotor,LOW);
+  }
+  
+  else{ 
+    digitalWrite(red,LOW);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,LOW); 
+    digitalWrite(buzzer,LOW);
+    digitalWrite(vibrationMotor,LOW);
+  }
+  
 }
